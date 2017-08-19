dc407cee3a4b09339f48d64afb5b82c41bf93643
John Rugis <jrugis@sc-math-316974.UoA.auckland.ac.nz>
2017-08-19 16:46:53 +1200

    initial commit

---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class move: MonoBehaviour
+{
+    [SerializeField]
+    private string[] m_Keywords;
+    private KeywordRecognizer m_Recognizer;
+
+    void Start()
+    {
+        m_Recognizer = new KeywordRecognizer(m_Keywords);
+        m_Recognizer.OnPhraseRecognized += OnPhraseRecognized;
+        m_Recognizer.Start();
+    }
+
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        if (args.text == m_Keywords[0])
+        {
+            transform.localPosition += new Vector3(0, 0.1F, 0); // move up
+        }
+        else if (args.text == m_Keywords[1])
+        {
+            transform.localPosition -= new Vector3(0, 0.1F, 0); // move down
+        }
+    }
+}
---
+++ Assets/Scripts/resize.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class resize: MonoBehaviour
+{
+    [SerializeField]
+    private string[] m_Keywords;
+    private KeywordRecognizer m_Recognizer;
+
+    Vector3 original_size;
+    Vector3 original_position;
+
+    void Start()
+    {
+        original_size = transform.localScale;
+        original_position = transform.localPosition;
+
+        m_Recognizer = new KeywordRecognizer(m_Keywords);
+        m_Recognizer.OnPhraseRecognized += OnPhraseRecognized;
+        m_Recognizer.Start();
+    }
+
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        if (args.text == m_Keywords[0])
+        {
+            transform.localScale *= 1.1F; // larger
+        }
+        else if (args.text == m_Keywords[1])
+        {
+            transform.localScale /= 1.1F; // smaller
+        }
+        if (args.text == m_Keywords[2])
+        {
+            transform.localScale *= 2.0F; //double size
+        }
+        else if (args.text == m_Keywords[3])
+        {
+            transform.localScale /= 2.0F; // half size
+        }
+        else if (args.text == m_Keywords[4])
+        {
+            transform.localScale = original_size; // original size
+            transform.localPosition = original_position; // original position
+        }
+    }
+}
