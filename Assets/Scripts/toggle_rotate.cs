db67ae91aee0582b6e9569231ec5fa7feb344245
John Rugis <jrugis@sc-math-316974.UoA.auckland.ac.nz>
2017-08-19 20:23:28 +1200

    added rotate

---
+++ Assets/Scripts/toggle_rotate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class toggle_rotate : MonoBehaviour
+{
+    [SerializeField]
+    private string[] m_Keywords;
+
+    private KeywordRecognizer m_Recognizer;
+
+    void Start()
+    {
+        m_Recognizer = new KeywordRecognizer(m_Keywords);
+        m_Recognizer.OnPhraseRecognized += OnPhraseRecognized;
+        m_Recognizer.Start();
+    }
+
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        GetComponent<rotate>().enabled = !GetComponent<rotate>().enabled;
+    }
+}
---
+++ Assets/Scripts/toggle_vis.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class toggle_vis : MonoBehaviour
+{
+    [SerializeField]
+    private string[] m_Keywords;
+
+    private KeywordRecognizer m_Recognizer;
+
+    void Start()
+    {
+        m_Recognizer = new KeywordRecognizer(m_Keywords);
+        m_Recognizer.OnPhraseRecognized += OnPhraseRecognized;
+        m_Recognizer.Start();
+    }
+
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        GetComponent<Renderer>().enabled = !GetComponent<Renderer>().enabled;
+    }
+}
