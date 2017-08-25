00223043a75fb2617664f0a95870069374e5c439
John Rugis <jrugis@sc-math-316974.UoA.auckland.ac.nz>
2017-08-25 21:00:36 +1200

    more voice commands

---
+++ Assets/Scripts/all_vis_on.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class all_vis_on: MonoBehaviour
+{
+    [SerializeField]
+    private string[] m_Keywords;
+
+    private KeywordRecognizer m_Recognizer;
+
+    void Start()
+    {
+        m_Recognizer = new KeywordRecognizer(m_Keywords);
+        m_Recognizer.OnPhraseRecognized += OnPhraseRecognized;
+        m_Recognizer.Start();
+    }
+
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        Renderer[] renders = GetComponentsInChildren<Renderer>();
+        foreach (Renderer rend in renders)
+            rend.enabled = true;
+    }
+}
