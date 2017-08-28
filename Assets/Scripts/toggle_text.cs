6f3ee2f45bb3764b924e2aadd4a2abb53b2ff6b9
John Rugis <jrugis@sc-math-316974.UoA.auckland.ac.nz>
2017-08-28 19:35:31 +1200

    added help

---
+++ Assets/Scripts/toggle_text.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class toggle_text : MonoBehaviour
+{
+    [SerializeField]
+    private string[] m_Keywords;
+
+    private KeywordRecognizer m_Recognizer;
+
+    void Start()
+    {
+        m_Recognizer = new KeywordRecognizer(m_Keywords);
+        m_Recognizer.OnPhraseRecognized += OnPhraseRecognized;
+        m_Recognizer.Start();
+    }
+
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        GetComponent<Text>().enabled = !GetComponent<Text>().enabled;
+    }
+}
